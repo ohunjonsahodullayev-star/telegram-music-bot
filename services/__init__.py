f05e166ee4c95251163e2b97e112488e26a80944
ohunjonsahodullayev-star <ohunjonsahodullayev@gmail.com>
2026-09-02 20:56:22 +0500

    Add services/__init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -8,18 +8,16 @@ from .downloader import (
     DownloaderTimeoutError,
     MediaResult,
     VideoUnavailableError,
-    download_audio,
     download_media,
     search_and_download_audio,
 )
 from .recognizer import TrackInfo, recognize_music
 
 __all__ = [
+    "MediaResult",
     "download_media",
-    "download_audio",
     "search_and_download_audio",
     "recognize_music",
-    "MediaResult",
     "TrackInfo",
     "DownloaderException",
     "VideoUnavailableError",
